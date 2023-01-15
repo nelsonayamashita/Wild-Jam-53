6c17571e1fbab24ee51fd6d81133ed60826bc24c
Nelson Yamashita <nelsonayamashita@gmail.com>
2023-01-14 21:19:43 -0300

    Add base Batller class

---
+++ game/battler/battler.gd
@@ -0,0 +1,9 @@
+extends Node
+class_name Battler
+
+@export var component_attack: Component
+@export var component_deffense: Component
+@export var component_special: Component
+@export var component_speed: Component
+@export var component_core: Component
+
